334d6826b3ccfb99c4643b6d045112d789d5360a
Stanislaw Hüll <st@nislaw.de>
2021-03-08 15:24:42 +0100

    logging now rocks

--- stockbro.py
+++ stockbro.py
@@ -47,11 +47,10 @@ def rss_fetch() -> int:
     return rows_after - rows_before
 
 
-# TODO: Remove functions and __name__ == ...; Use executable script directly
+## Command-line argument configuration.
+#
+# Declares each subcommand and declares its signature.
 
-#
-# Parse command-line arguments
-#
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_verbosity_meg = parser.add_mutually_exclusive_group()
 parser_verbosity_meg.add_argument("-v", "--verbose", action="store_true", default=False,
@@ -64,44 +63,60 @@ subparsers = parser.add_subparsers(help="command help", dest="command")
 parser_fetch_rss_feeds = subparsers.add_parser("rss-fetch", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser_download_html = subparsers.add_parser("rss-download-html", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_download_html.add_argument("-m", "--maxitems", type=int, default=128,  #FIXME: enfore nonneg integers
+parser_download_html.add_argument("-m", "--maxitems", type=int, default=128,  # FIXME: enforce nonneg integers
                                   help="Stop after given number of items have been processed. Used to chunk up "
                                        "workload into batches of predictable duration.")
 
 parser_extract_fulltext = subparsers.add_parser("rss-extract-fulltext", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_extract_fulltext.add_argument("-m", "--maxitems", type=int, default=128,  # FIXME: enforce nonneg integers
+                                     help="Stop after given number of items have been processed. Used to chunk up "
+                                          "workload into batches of predictable duration.")
 
 args = parser.parse_args()
 cfg = configparser.ConfigParser(inline_comment_prefixes=";")
 cfg.read("setup.cfg")
 
-# Configure logging. Affects code in all child modules. Logs are always
-# stored at full maximum information in the logfile, but the log level may
-# be selectively selected for terminal output.
+## Logging configuration.
+#
+# All logging output including that from imported modules is saved to file at
+# DEBUG-level verbosity.  Additionally, logging output of this project's own
+# modules is sent to console. The console output's verbosity can be adjusted
+# via command-line flags (-v / -q).
+
+# Root logger setup. Writes ALL logging output to file.
 logdir = Path(cfg["project"]["logdir"])
 if not logdir.is_dir():
     logdir.mkdir(parents=True, exist_ok=True)
 logfilename = str(datetime.now().strftime(r"%Y-%m-%d")) + ".log"
 logpath = str(logdir / logfilename)  # log-directory/YYYY-MM-DD.log; one logfile per day
-
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
 fh = logging.FileHandler(logpath)
 fh.setFormatter(logging.Formatter('[%(levelname)s] ⌚ %(asctime)s - %(name)s - %(funcName)s: %(message)s'))
 root_logger.addHandler(fh)
 
+# Module-level logger. Writes local logging output to console. This logger
+# shall be used within every child module like so:
+#
+#     import logging
+#     log = logging.getLogger("stockbro")
+#     log.info("...")
+#
 log = logging.getLogger("stockbro")
-if args.quiet:
-    log.setLevel(logging.WARNING)
-elif args.verbose:
-    log.setLevel(logging.DEBUG)
-else:
-    log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
+if args.quiet:
+    ch.setLevel(logging.WARNING)
+elif args.verbose:
+    ch.setLevel(logging.DEBUG)
+else:
+    ch.setLevel(logging.INFO)
 ch.setFormatter(logging.Formatter('[%(levelname)s] %(funcName)s: %(message)s'))
 log.addHandler(ch)
 
 
-# Dispatch command
+## Command dispatch.
+
 if args.command == "rss-fetch":
     # TOOD: Remove rss_fetch() and place implementation here
     rows_created = rss_fetch()
@@ -114,13 +129,21 @@ elif args.command == "rss-extract-fulltext":
 
     conn_feeds = sqlite3.connect(cfg["project"]["rss-feedsdb-path"])
     conn_catalog = sqlite3.connect(cfg["project"]["rss-catalogdb-path"])
-    query_join = "SELECT items.rss_guid, items.rss_link, rss_pubdate, rss_title, rss_description FROM " \
-        "items LEFT JOIN progress ON (items.rss_guid = progress.rss_guid AND items.rss_link = progress.rss_link) " \
-        "WHERE (can_delete IS NULL OR can_delete != 1)"
-    for row in conn_feeds.execute(query_join):
-        link = row[1]
-        title = row[3]
-        description = row[4]
+    # query_join = "SELECT items.rss_guid, items.rss_link, rss_pubdate, rss_title, rss_description FROM " \
+    #     "items LEFT JOIN progress ON (items.rss_guid = progress.rss_guid AND items.rss_link = progress.rss_link) " \
+    #     "WHERE (can_delete IS NULL OR can_delete != 1)"
+    query_join = """
+        SELECT rss_guid, rss_link, rss_pubdate, rss_title, rss_description, dest_url, html from
+            (
+                SELECT rss_guid, rss_link, rss_pubdate, rss_title, rss_description FROM items LEFT JOIN progress
+                    USING(rss_guid, rss_link) WHERE progress.can_delete IS NULL
+            ) LEFT JOIN html USING(rss_guid, rss_link) WHERE html IS NOT NULL
+        """
+    for record in conn_feeds.execute(query_join).fetchmany(args.maxitems):
+        link = record[1]
+        title = record[3]
+        description = record[4]
+        # fulltext = rss.extract_fulltext(html, url)
         # CONTINUEHERE
         #  1. copy: rss-feeds::items::rss_link -> rss-catalog::items::link
         #           rss_pubdate -> pubdate
@@ -128,7 +151,6 @@ elif args.command == "rss-extract-fulltext":
         #  2. pubdate zu standardisiertem Format konvertieren -> pubdate
         #  3. volltext extrahieren -> fulltext
         #  4. rss-feeds::progress::can_delete = 1
-        print(str(row))
 
     conn_feeds.commit()
     conn_feeds.close()
